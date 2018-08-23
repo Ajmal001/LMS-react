12b1b156af30f2cac9bc8a65c78d97e9182b713f
webdozen <microlion116@gmail.com>
2018-08-24 10:15:34 +1100

    Added google calendar placeholder

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 Y=2018
 M=8
-D=23
+D=24
 i=15
-comment=" Added placeholders for calendar, grades and help"
+comment=" Added google calendar placeholder"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
