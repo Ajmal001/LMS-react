4bf71dc7c43d7a85c447e38bf852b3aab220489e
webdozen <microlion116@gmail.com>
2018-09-01 10:15:34 +1100

    Removed express server. We donΓÇÖt need it, as Firebase hosting already has one

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
 Y=2018
-M=8
-D=24
+M=9
+D=1
 i=15
-comment=" Added google calendar placeholder"
+comment="Removed express server. We donΓÇÖt need it, as Firebase hosting already has one
+ "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
