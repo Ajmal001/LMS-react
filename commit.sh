b19176b349a80d2949f476cd877642804df9a2fe
webdozen <microlion116@gmail.com>
2018-09-11 10:16:34 +1100

    Fixed issue with infinite loop in select2 onChange

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2018
 M=9
 D=11
 i=16
-comment=" Refactored selects into select2"
+comment=" Fixed issue with infinite loop in select2 onChange"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
