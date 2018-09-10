626380887a06773834d7b003c0dc0b72b7f5b899
webdozen <microlion116@gmail.com>
2018-09-11 10:16:34 +1100

    Added admin screenshot

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2018
 M=9
 D=11
 i=16
-comment=" Fixed issue with infinite loop in select2 onChange"
+comment=" Added admin screenshot"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
