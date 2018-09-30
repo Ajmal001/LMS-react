5e3ae9285647bd5ffbaeaa4bf5ceec258dadc83d
webdozen <microlion116@gmail.com>
2018-10-01 10:16:34 +1100

    Redesigning home page and top nav

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
 Y=2018
-M=9
-D=11
+M=10
+D=1
 i=16
-comment=" Added admin screenshot"
+comment=" Redesigning home page 
+and top nav"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
