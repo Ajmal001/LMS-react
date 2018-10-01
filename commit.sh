b653f837a63d333050361c05787d0d794e382ea8
webdozen <microlion116@gmail.com>
2018-10-02 10:16:34 +1100

    working on home hero

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2018
 M=10
 D=2
 i=16
-comment=" Added circles to map"
+comment=" working on home hero "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
