fcc7124888661087ddefb38b35c82b3e82557bf2
webdozen <microlion116@gmail.com>
2018-10-02 10:16:34 +1100

    Added circles to map

--- commit.sh
+++ commit.sh
@@ -1,9 +1,8 @@
 Y=2018
 M=10
-D=1
+D=2
 i=16
-comment=" Redesigning home page 
-and top nav"
+comment=" Added circles to map"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
