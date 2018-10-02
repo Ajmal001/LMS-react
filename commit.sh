f3561af95b86213070e367cc59da3c392873eeb4
webdozen <microlion116@gmail.com>
2018-10-03 10:16:34 +1100

    working on home page and admin

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 Y=2018
 M=10
-D=2
+D=3
 i=16
-comment=" working on home hero "
+comment=" working on home page and admin "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
