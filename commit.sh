a15d43a851ad7047bc1b433e608b51ddf48aa800
webdozen <microlion116@gmail.com>
2018-10-03 10:16:34 +1100

    Fixed home grid with flex layout

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2018
 M=10
 D=3
 i=16
-comment=" working on home page and admin "
+comment=" Fixed home grid with flex layout "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
