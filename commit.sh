4d828f4f8ba351ec40c21dee5650be02d6adad53
webdozen <microlion116@gmail.com>
2018-10-04 10:16:34 +1100

    working on file manager

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 Y=2018
 M=10
-D=3
+D=4
 i=16
-comment=" Fixed home grid with flex layout "
+comment=" working on file manager "
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
